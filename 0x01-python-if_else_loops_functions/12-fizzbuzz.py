41d2039bf6e14b3a9d8f534133d4212781c8c273
Byrone Paul <otienobyrone68@gmail.com>
2021-09-08 10:47:18 +0300

    comb

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print("{} is positive".format(number))
-elif number == 0:
-    print("{} is zero".format(number))
-else:
-    print("{} is negative".format(number))
+# 12-fizzbuzz.py
+
+
+def fizzbuzz():
+    """Print the numbers from 1 to 100 separated by a space.
+    For multiples of three, print Fizz instead of the number.
+    For multiples of five, print Buzz instead of the number.
+    For multiples of three and five, print FizzBuzz instead of the number.
+    """
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
